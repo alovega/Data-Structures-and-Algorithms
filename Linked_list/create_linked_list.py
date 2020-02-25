c03721ad29363e3ab5d0f5565103a13072e8abca
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2020-02-25 16:08:47 +0300

    first commit

---
+++ Linked_list/create_linked_list.py
@@ -0,0 +1,190 @@
+
+class Node:
+	"""
+	A node in a linked list consist of an item value and the pointer/ reference to the next item
+	"""
+	def __init__(self, data):
+		self.item = data
+		self.ref = None
+
+
+class LinkedList:
+	"""
+	Will  contain one member start_node that will point to the start or the first node of the linked list
+	"""
+	def __init__(self):
+		self.start_node = None
+
+	def traverse_list(self):
+		"""
+		this is a function for traversing and returning values of a linked list
+		:return:
+		"""
+		if self.start_node is None:
+			print "List has no elements"
+			return
+		else:
+			n = self.start_node
+			while n is not None:
+				print n.item, ''
+				n = n.ref
+
+	def insert_at_start(self, data):
+		"""
+		inserts an item at the beginning of the node and thus the item becomes the start node
+		:param data:
+		:return:
+		"""
+		new_node = Node(data)
+		new_node.ref = self.start_node
+		self.start_node = new_node
+
+	def insert_at_end(self, data):
+		"""
+		Inserting an Item at the end of the linked list
+		:param data:
+		:return:
+		"""
+		new_node = Node(data)
+		if self.start_node is None:
+			self.start_node = new_node
+			return
+		n = self.start_node
+		while n.ref is not None:
+			n = n.ref
+		n.ref = new_node
+
+	def insert_after_item(self, x, data):
+		"""
+		inserting an item after a given item x
+		:param x: value of the node that will point to the inserted data
+		:param data:
+		:return:
+		"""
+		n = self.start_node
+		print(n.ref)
+		while n is not None:
+			if n.item == x:
+				break
+			n = n.ref
+		if n is None:
+			print "Item not in list"
+		else:
+			new_node = Node(data)
+			new_node.ref = n.ref
+			n.ref = new_node
+
+	def insert_before_item(self, x, data):
+		"""
+		inserting an item before a given item
+		:param x: the item given for you to insert before it
+		:param data:
+		:return:
+		"""
+		if self.start_node is None:
+			print "The list has no items"
+			return
+		if x == self.start_node.item:
+			new_node = Node(data)
+			new_node.ref = self.start_node
+			self.start_node = new_node
+			return
+		n = self.start_node
+		while n.ref is not None:
+			if n.ref.item == x:
+				break
+			n = n.ref
+		if n.ref is None:
+			print "item not in list"
+		else:
+			new_node = Node(data)
+			new_node.ref = n.ref
+			n.ref = new_node
+
+	def insert_at_index(self, index, data):
+		if index == 1:
+			new_node = Node(data)
+			new_node.ref = self.start_node.ref
+			self.start_node = new_node
+		i = 1
+		n = self.start_node
+		while i < index-1 and n is not None:
+			n = n.ref
+			i +=1
+		if n is None:
+			print "Index out of bound"
+		else:
+			new_node = Node(data)
+			new_node.ref = n.ref
+			n.ref = new_node
+
+	def get_count(self):
+		"""
+		counts the number of elements in a linked list
+		:return:
+		"""
+		if self.start_node is None:
+			return 0
+		n = self.start_node
+		count = 0
+		while n is not None:
+			count = count + 1
+			n = n.ref
+		return count
+
+	def search_item(self, x):
+		"""
+		for searching if an item exist in a linked linked list
+		:param x: the item to be searched
+		:return: boolean
+		"""
+		if self.start_node is None:
+			print "List has no elements"
+			return
+		n = self.start_node
+		while n is not None:
+			if n.item == x:
+				print "Item found"
+				return True
+			n = n.ref
+		print "Item not found"
+		return False
+
+	def make_new_list(self):
+		"""
+		A function for creating a new linked list
+		:return:
+		"""
+		nums = int(input("How many nodes do you want to create: "))
+		if nums == 0:
+			return
+		for i in range(nums):
+			value = int(input("Enter the value for the node: "))
+			self.insert_at_end(value)
+
+	def delete_at_start(self):
+		"""
+		delete an element at the start of the linked list
+		:return:
+		"""
+		if self.start_node is None:
+			print "The list has no element to delete"
+			return
+		self.start_node = self.start_node.ref
+
+	def delete_at_end(self):
+		"""
+		delete an element at the end of the linked list
+		:return:
+		"""
+		if self.start_node is None:
+			print "the list has no element to delete"
+			return
+		n = self.start_node
+		while n.ref.ref is not None:
+			n = n.ref
+		n.ref = None
+
+
+
+
---
+++ ReadMe.md
@@ -0,0 +1,20 @@
+#Data Structures
+
+###Linked List
+```
+In arrays  data is stored in contigous memory location data is stored in contigous memory location for instance if
+ the first item is stored at index 10 and is of size 15 bytes the next item will be stored at location 10 + 15 + 1
+
+But for linked list data is not stored at contigous memory location for each item in memory location linked list
+ stores the value of the item and the reference or pointer to the next item which constitutes of a node
+```
+#####Advantages of using a lined list
+```
+1. A linked list is a dynamic datastructure which means memory reserved for it can be reduced or increased at run time.
+
+2. Since Arrays require contigous memory location it is very difficult to insert or delete an item  because the
+ memory location of large items have to updated. Whereas in a linked list items are not stored in contigous memory
+ location therefore you can easily update or delete an item
+
+3. Owing to its flexibility, a linked list is more suitable for implementing data structures like stacks, queues, and lists.
+```
